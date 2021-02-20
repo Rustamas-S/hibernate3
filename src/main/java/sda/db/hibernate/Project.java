705d397bacad1b9cb5d3f5d3644abb032c5adbee
Kęstutis Triponis <kestutis.triponis@gmail.com>
2021-02-20 11:02:23 +0100

    Simple commit

--- src/main/java/sda/db/hibernate/Project.java
+++ src/main/java/sda/db/hibernate/Project.java
@@ -1,12 +1,11 @@
 package sda.db.hibernate;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
 import sda.db.hibernate.entity.Song;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import java.util.List;
 
 public class Project {
@@ -18,6 +17,15 @@ public class Project {
                 .buildSessionFactory();
 
         EntityManager em = sessionFactory.createEntityManager();
+
+        Song song = new Song();
+        song.setName("test");
+
+        EntityTransaction t = em.getTransaction();
+
+        t.begin();
+        em.persist(song);
+        t.commit();
 
         List<Song> songs = em.createQuery("FROM Song s", Song.class).getResultList();
         songs.forEach(System.out::println);
--- src/main/java/sda/db/hibernate/entity/Song.java
+++ src/main/java/sda/db/hibernate/entity/Song.java
@@ -14,11 +14,29 @@ public class Song {
     @GeneratedValue()
     private UUID id;
 
+    private String name;
+
     public UUID getId() {
         return id;
     }
 
     public void setId(UUID id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
