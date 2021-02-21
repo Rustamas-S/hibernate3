e602d4e1becca52912cdaf07132a16a9cefaf608
Kęstutis Triponis <kestutis.triponis@gmail.com>
2021-02-21 12:34:57 +0100

    Restructure Project

--- src/main/java/sda/db/hibernate/Project.java
+++ src/main/java/sda/db/hibernate/Project.java
@@ -13,13 +13,17 @@ import sda.db.hibernate.repository.AuthorRepository;
 import sda.db.hibernate.repository.SongRepository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
 import java.time.Instant;
 import java.util.List;
 
 public class Project {
 
-    public void run() {
+    private final AgentRepository agentRepository;
+    private final AlbumRepository albumRepository;
+    private final AuthorRepository authorRepository;
+    private final SongRepository songRepository;
+
+    public Project() {
         SessionFactory sessionFactory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Agent.class)
@@ -28,26 +32,29 @@ public class Project {
                 .addAnnotatedClass(Song.class)
                 .buildSessionFactory();
 
-        EntityManager em = sessionFactory.createEntityManager();
-        AgentRepository agentRepository = new AgentRepository(em);
-        AlbumRepository albumRepository = new AlbumRepository(em);
-        AuthorRepository authorRepository = new AuthorRepository(em);
-        SongRepository songRepository = new SongRepository(em);
+        EntityManager entityManager = sessionFactory.createEntityManager();
 
-        EntityTransaction t = em.getTransaction();
+        agentRepository = new AgentRepository(entityManager);
+        albumRepository = new AlbumRepository(entityManager);
+        authorRepository = new AuthorRepository(entityManager);
+        songRepository = new SongRepository(entityManager);
+    }
 
-        t.begin();
-
+    public void run() {
         Author author = new Author();
         author.setName("Super Author");
+        authorRepository.save(author);
 
         Song songD = new Song("song D", author, 123, Instant.now());
+        songRepository.save(songD);
 
         Album albumA = createAlbumA(author);
         albumA.addSong(songD);
+        albumRepository.save(albumA);
 
         Album albumB = createAlbumB(author);
         albumB.addSong(songD);
+        albumRepository.save(albumB);
 
         Agent agent = new Agent();
         agent.setId(new AgentId("Vardenis", "Pavardenis"));
@@ -55,26 +62,7 @@ public class Project {
         author.setAgent(agent);
         agentRepository.save(agent);
 
-        authorRepository.save(author);
-        albumRepository.save(albumA);
-        albumRepository.save(albumB);
-
-        t.commit();
-
-        System.out.println("=========");
-        List<Song> songs = songRepository.findAll();
-        songs.forEach(System.out::println);
-        System.out.println("=========");
-        Song songA = songs.stream().findFirst().get();
-        songA.setLyrics("some other lyrics");
-        songRepository.save(songA);
-        System.out.println(songRepository.find(songA.getId()));
-        songRepository.delete(songA);
-        System.out.println(songRepository.find(songA.getId()));
-        System.out.println("=========");
-        agentRepository.findAll().forEach(System.out::println);
-        System.out.println("=========");
-        System.out.println(agentRepository.find("Vardenis", "Pavardenis"));
+        print();
     }
 
     private Album createAlbumA(Author author) {
@@ -101,5 +89,22 @@ public class Project {
         album.addSong(songC);
 
         return album;
+    }
+
+    private void print() {
+        System.out.println("=========");
+        List<Song> songs = songRepository.findAll();
+        songs.forEach(System.out::println);
+        System.out.println("=========");
+        Song songA = songs.stream().findFirst().get();
+        songA.setLyrics("some other lyrics");
+        songRepository.save(songA);
+        System.out.println(songRepository.find(songA.getId()));
+        songRepository.delete(songA);
+        System.out.println(songRepository.find(songA.getId()));
+        System.out.println("=========");
+        agentRepository.findAll().forEach(System.out::println);
+        System.out.println("=========");
+        System.out.println(agentRepository.find("Vardenis", "Pavardenis"));
     }
 }
